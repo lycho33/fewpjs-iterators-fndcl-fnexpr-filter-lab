beb41589766fdbfe56d490d5f05a1335ee5c815c
lycho33 <cho.lyds@gmail.com>
2021-08-31 17:16:18 -0400

    commit tree baeb987bda75719398d084df5d1507502fa46c1a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+let findMatching = (drivers, string) => {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, string) => {
+    console.log(string)
+    return drivers.filter(driver => driver.charAt(0) === string.charAt(0))
+}
+
+let matchName = (drivers, string) => {
+    return drivers.filter(driver => driver.name === string)
+}
